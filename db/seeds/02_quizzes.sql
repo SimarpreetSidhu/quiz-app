34308e31da7a3dc8d4e0a3605ee217b687b0f346
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-07-15 23:00:54 -0400

    fix error related to seeding quizzes table

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,5 +1,5 @@
 -- Seed Quizzes
-INSERT INTO quizzes (quiz_title, quiz_description, creator_id, visibility, created_time, sharable_url)
+INSERT INTO quizzes (quiz_title, quiz_description, creator_id, visibility, created_at, sharable_url)
 VALUES 
   ('JavaScript Basics', 'A quiz on JS fundamentals', 1, true, NOW(), 'js-basics-123'),
   ('Python Intro', 'Test your Python knowledge', 2, false, NOW(), 'py-intro-456');
