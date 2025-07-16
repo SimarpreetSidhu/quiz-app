2e3ac024efd21594396b86be5dccb6df1ed415cc
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-15 17:58:14 -0700

    create quiz table

---
+++ db/schema/02_quiz.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS quiz CASCADE;
+CREATE TABLE quiz (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_title TEXT NOT NULL,
+  quiz_description TEXT,
+  creator_id INTEGER NOT NULL REFERENCES user(id),
+  visibility BOOLEAN NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  sharable_url TEXT
+);
