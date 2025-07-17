e65f472d7e2c5ecef1da1158eed7d9bca75acfac
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-16 20:41:35 -0700

    add error reading

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
 const { getPublicQuizzes } = require('../db/queries/quiz');
 
 router.get('/home', (req, res) => {
   getPublicQuizzes()
     .then(quizzes => {
       res.render('home', { quizzes });
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
     });
 });
 
+module.exports = router;
