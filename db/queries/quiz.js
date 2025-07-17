319b3580d871ac1f2a01ea342beae2fb9792bd0d
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-17 14:30:49 -0700

    add quiz description above the quiz link

--- db/queries/quiz.js
+++ db/queries/quiz.js
@@ -1,7 +1,12 @@
 const db = require('../connection');
 
 const getPublicQuizzes = () => {
-  return db.query('SELECT * FROM quizzes WHERE visibility = true;')
+  return db.query(`
+    SELECT quizzes.id, quizzes.quiz_title, quizzes.quiz_description, users.username AS creator_name
+    FROM quizzes
+    JOIN users ON quizzes.creator_id = users.id
+    WHERE quizzes.visibility = true;
+    `)
     .then(res => res.rows);
 };
 
