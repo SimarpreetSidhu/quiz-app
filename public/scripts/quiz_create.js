c04f0399b914308835bd8afcaa9b9b0a08162fdc
Jason Chang <changjason96@gmail.com>
2025-07-18 12:12:47 -0400

    created function to add all questions and answers into an object

--- public/scripts/quiz_create.js
+++ public/scripts/quiz_create.js
@@ -1,8 +1,27 @@
-let num = 0;
+
+const questionGather = () => {
+  let questionCount = 0
+  $('.question-container').each(()=>{
+    questionCount= $('.question-container').find('.question').length;
+  });
+  let questionObject = {};
+  for (let i = 0; i < questionCount - 1 ; i++)
+  {
+    console.log(questionCount);
+    questionObject[i]= {};
+    questionObject[i].question = $(`#${i}`).find("textarea").val();
+    questionObject[i].answeer = $(`#${i}`).find(`input[name="answer${i}"]:checked`).next('label').text();
+  }
+console.log(questionObject);
+}
+
+
+
+let num = 1;
 $('.question-container').on('click', '.add', function(e) {
   e.preventDefault();
   $('.question:last').before(
-    `<div class="question">
+    `<div class="question" id="${num}">
         <textarea>Insert Question here</textarea>
         <input type="radio" id="true" name="answer${num}">
         <label for="true">True</label>
@@ -23,6 +42,6 @@ $('.question-container').on('click','.save', function(e){
   var quizName =$('#quiz-name').val();
   $('.quiz-name').text(quizName);
   var descriptionText =$('.description-text').val();
-  console.log(descriptionText);
   $('.description-text').text(descriptionText);
+  questionGather();
 })
