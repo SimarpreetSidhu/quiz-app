65a586fd91a02e0a75a68ddac7a4f2187fb8d766
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-20 16:21:39 -0700

    added more questions into quiz

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -4,4 +4,6 @@ VALUES
   (1, 'Is JavaScript case-sensitive?', 1, true),
   (1, 'Does "==" check type in JS?', 2, false),
   (2, 'Is Python dynamically typed?', 1, true),
-  (2, 'Does Python use curly braces for blocks?', 2, false);
+  (2, 'Does Python use curly braces for blocks?', 2, false),
+  (3, 'Was JavaScript created in just 10 days?', 3, true),
+  (3, 'JavaScript is the same as Java.', 4, false);
