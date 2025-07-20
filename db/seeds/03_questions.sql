b6c48a1bcfc6338e9e4afa42e41818416a77f1a9
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-20 16:29:08 -0700

    added more questions into quiz

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -5,5 +5,7 @@ VALUES
   (1, 'Does "==" check type in JS?', 2, false),
   (2, 'Is Python dynamically typed?', 1, true),
   (2, 'Does Python use curly braces for blocks?', 2, false),
-  (3, 'Was JavaScript created in just 10 days?', 3, true),
-  (3, 'JavaScript is the same as Java.', 4, false);
+  (3, 'Was JavaScript created in just 10 days?', 1, true),
+  (3, 'JavaScript is the same as Java.', 2, false),
+  (3, 'Can JavaScript run outside the browser using Node.js?', 3, true),
+  (3, 'Is JavaScript statically typed?', 4, false);
