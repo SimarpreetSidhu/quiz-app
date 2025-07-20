c64d2866320d0b19da2f26a71b666d963b6d5569
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-20 14:31:00 -0700

    created route to submit answers by user

--- routes/quiz.js
+++ routes/quiz.js
@@ -32,8 +32,37 @@ router.get('/:id/attempt', (req, res) => {
     });
 });
 
+//POST route to submit answers
+router.post('/quiz/:id/submit', (req, res) => {
+  const userAnswers = req.body; // the answers that user selected
+  const quizId = req.params.id;
+  let score = 0;
+  //retrieve correct answers for quiz from DB
+  getQuestionsByQuizId(quizId)
+  .then(questions => {
+    questions.forEach(question => {
+      const userAnswer = userAnswers[`question-${question.id}`];
+      //count correct answers and score
+      if (userAnswer === String(question.correct_answer)) {
+        score++;
+      }
+    });
+
+    // Render the result of score page after checking all answers
+    res.render('result', {
+      score,
+      total: questions.length
+    });
+  })
+  .catch(err => {
+    console.error("Error submitting quiz:", err);
+    res.status(500).send("Something went wrong!");
+  });
+});
+
 router.get('/new', (req, res) => {
   res.render('quiz_new');
 });
+
 
 module.exports = router;
