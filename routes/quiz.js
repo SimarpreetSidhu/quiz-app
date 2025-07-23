4df459108ad2bdc40acc27b80ced7ada69f07d6e
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-22 22:19:36 -0700

    add share the link feature in dashboard

--- routes/quiz.js
+++ routes/quiz.js
@@ -27,18 +27,15 @@ router.get('/', (req, res) => {
 //render quiz.ejs and pass questions with the specific quizId.
 router.get('/:id/attempt', (req, res) => {
   const quizId = req.params.id;
+  const shareUrl = `${req.protocol}://${req.get('host')}`;
   //show quiz info & the questions
   Promise.all([getQuizById(quizId), getQuestionsByQuizId(quizId)])
     .then(([quiz, questions]) => {
-      res.render('quiz', { 
-        questions, 
-        quiz, 
-        quizId
+      res.render('quiz', { questions, quiz, quizId, shareUrl });
     })
     .catch((err) => {
       res.status(500).send('Error loading quiz questions: ' + err.message);
     });
-  });
 });
 
 //POST route to submit answers
